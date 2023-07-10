3eb1fa9633d5c0b2a273373ff5134150993fcb23
Gabriel <gabrielsenese6@gmail.com>
2023-07-10 16:17:33 -0400

    Initial commit

---
+++ js/test.js
@@ -0,0 +1,10 @@
+function getUserData(){
+    var inputMinAmount = document.getElementById("minamount");
+    console.log(inputMinAmount.value);
+    var inputMaxAmount = document.getElementById("maxamount");
+    console.log(inputMaxAmount.value);
+    //var valueMin = inputMinAmount.value;
+    //var valueMax = inputMaxAmount.value;
+    //console.log(valueMin);
+    //console.log(valueMax);
+}
